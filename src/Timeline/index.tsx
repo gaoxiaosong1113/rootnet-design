23ed4472b3952a0798d99c72e718e70ae4ba08c3
gaoxs <gaoxiaosong@croot.com>
2021-07-09 12:59:26 +0800

    修改基础dome，优化细节

--- src/Timeline/index.tsx
+++ src/Timeline/index.tsx
@@ -1,11 +1,18 @@
+// 引入react依赖
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+// 引入第三方依赖
+import { CSSTransitionGroup } from 'react-transition-group'; // ES6
 import clsx from 'clsx';
 
+// 引入样式
 import './index.less';
 
+// 引入配置文件
 import { prefix } from '../config';
 
+// 引入组件
 import { Icon } from '../index';
 
 export interface TimelineProps {
