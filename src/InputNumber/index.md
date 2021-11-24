1a8745551dc1bb140f01850eb0dac75b7cbadb1f
anhaogxs <704041637@qq.com>
2021-11-24 15:28:38 +0800

    增加inputNumber组件

--- src/InputNumber/index.md
+++ src/InputNumber/index.md
@@ -12,8 +12,254 @@ group:
 示例：
 
 ```tsx
-import React from 'react';
-import { Foo } from 'rootnet-design';
+import React, { useState } from 'react';
+import { InputNumber, Button, Grid } from 'rootnet-design';
 
-export default () => <span>First Demo</span>;
+const { Row, Col } = Grid;
+
+export default () => {
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(e) => {
+              console.log(e);
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 ```
+
+附加信息：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber placeholder="请输入内容" after={'%'} />
+        </Col>
+        <Col span={8}>
+          <InputNumber placeholder="请输入内容" before={'金额'} after={'%'} />
+        </Col>
+        <Col span={8}>
+          <InputNumber placeholder="请输入内容" before={<Icon name="mima1" />} after={'%'} />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+设置图标：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            icon={<Icon name="mima1" />}
+            close
+            affterIcon={<Icon name="mima1" />}
+            onChange={(e) => {
+              console.log(e);
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+非受控：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(value) => {
+              console.log(value);
+            }}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+设置默认值：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  const [value, setValue] = useState(55);
+
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(v) => setValue(v)}
+            value={value}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+设置默认值：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  const [value, setValue] = useState(55);
+
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(v) => setValue(v)}
+            value={value}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+        <Col span={8}>
+          <InputNumber placeholder="请输入内容" icon={<Icon name="mima1" />} />
+        </Col>
+        <Col span={8}>
+          <InputNumber placeholder="请输入内容" before={<Icon name="mima1" />} after={'%'} />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+设置最大长度：
+
+```tsx
+import React, { useState } from 'react';
+import { InputNumber, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  const [value, setValue] = useState(55);
+
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(v) => setValue(v)}
+            value={value}
+            maxLength={4}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onChange={(v) => setValue(v)}
+            value={value}
+            maxLength={4}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+        <Col span={8}>
+          <InputNumber
+            placeholder="请输入内容"
+            onInput={(v) => console.log(v)}
+            value={value}
+            maxLength={4}
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+禁用：
+
+```tsx
+import React, { useState } from 'react';
+import { Input, Icon, Button, Grid } from 'rootnet-design';
+
+const { Row, Col } = Grid;
+
+export default () => {
+  const [value, setValue] = useState(55);
+
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Input
+            placeholder="请输入内容"
+            onChange={(v) => setValue(v)}
+            value={value}
+            disabled
+            icon={<Icon name="mima1" />}
+          />
+        </Col>
+        <Col span={8}>
+          <Input disabled placeholder="请输入内容" icon={<Icon name="mima1" />} />
+        </Col>
+        <Col span={8}>
+          <Input placeholder="请输入内容" before={<Icon name="mima1" />} after={'%'} disabled />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+```
+
+<API />
--- src/InputNumber/index.tsx
+++ src/InputNumber/index.tsx
@@ -1,5 +1,5 @@
 // 引入react依赖
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState, useRef, useEffect, useImperativeHandle } from 'react';
 import ReactDOM from 'react-dom';
 
 // 引入第三方依赖
@@ -13,7 +13,7 @@ import './index.less';
 import { prefix } from '../config';
 
 // 引入组件
-import { Icon } from '../index';
+import { Icon, Input } from '../index';
 
 export interface InputNumberProps {
   /**
@@ -25,76 +25,232 @@ export interface InputNumberProps {
   children?: ReactNode;
 
   /**
-   * @description      按钮的类型
-   * @default           -
-   */
-  type?: string;
-
-  /**
    * @description      需要显示的图标
    * @default           -
    */
-  icon?: string;
+  icon?: any;
 
   /**
-   * @description      是否禁用按钮
+   * @description      input 后面需要显示的图标
+   * @default           -
+   */
+  affterIcon?: any;
+
+  /**
+   * @description      是否禁用输入框
    * @default           false
    */
   disabled?: boolean;
 
   /**
-   * @description      InputNumber点击事件
-   * @default           -
-   */
-  onClick?: Function;
-
-  /**
-   * @description      InputNumber左右的间隔
-   * @default           -
-   */
-  interval?: string;
-
-  /**
-   * @description      InputNumber的尺寸
+   * @description      Input的尺寸
    * @default           -
    */
   size?: string;
+
+  /**
+   * @description      提示文字
+   * @default           -
+   */
+  placeholder?: string;
+
+  /**
+   * @description      带标签的 input，设置后置标签
+   * @default           -
+   */
+  after?: any;
+
+  /**
+   * @description      带标签的 input，设置前置标签
+   * @default           -
+   */
+  before?: any;
+
+  /**
+   * @description      输入框内容变化时的回调
+   * @default           -
+   */
+  onChange?: Function;
+
+  /**
+   * @description      输入框获取焦点的回调
+   * @default           -
+   */
+  onFocus?: Function;
+
+  /**
+   * @description      输入框触发事件的回调
+   * @default           -
+   */
+  onInput?: Function;
+
+  /**
+   * @description      输入框失去焦点的回调
+   * @default           -
+   */
+  onBlur?: Function;
+
+  /**
+   * @description      name
+   * @default           -
+   */
+  name?: string;
+
+  /**
+   * @description      输入框内容
+   * @default           -
+   */
+  value?: number;
+
+  /**
+   * @description      是否默认获取焦点
+   * @default           -
+   */
+  focus?: boolean;
+
+  /**
+   * @description      是否设置最大长度
+   * @default           -
+   */
+  maxLength?: number;
+
+  /**
+   * @description      启用自动完成功能的表单
+   * @default           -
+   */
+  autoComplete?: string;
+
+  /**
+   * @description      步长
+   * @default           1
+   */
+  step?: number;
 }
 
-function InputNumber(props: InputNumberProps) {
+function InputNumber(props: InputNumberProps, ref: any) {
   const {
     className,
-    type,
+    style,
+    placeholder,
+    before,
+    after,
     icon,
+    affterIcon,
+    onChange,
+    onFocus,
+    onBlur,
+    name,
     disabled,
-    children,
-    onClick,
-    interval,
-    size,
+    maxLength,
+    onInput,
+    step = 1,
     ...prop
   } = props;
 
-  function handleClick() {
-    if (!disabled && onClick) {
-      onClick();
+  const [value, setValue] = useState(props.value || '');
+  const [focus, setFocus] = useState(props.focus);
+
+  const refEl = useRef(null) as any;
+
+  useImperativeHandle(ref, () => refEl.current);
+
+  const handleChange = (e: any) => {
+    let val = e.target.value;
+    if (isNaN(Number(val))) return;
+    if (maxLength && val.length > maxLength) e.target.value = val.slice(0, maxLength);
+    setValue(e.target.value);
+    if (onChange) {
+      onChange(e.target.value, e);
     }
-  }
+  };
+
+  const handleFocus = (e: any) => {
+    setFocus(true);
+    if (onFocus) {
+      onFocus(value, e);
+    }
+    return false;
+  };
+
+  const handleBlur = (e: any) => {
+    setFocus(false);
+    if (onBlur) {
+      onBlur(value, e);
+    }
+  };
+
+  const handleInput = (e: any) => {
+    setFocus(false);
+    if (onInput) {
+      onInput(value, e);
+    }
+  };
+
+  const handleUp = () => {
+    if (disabled) return;
+    setValue(Number(value) + step);
+  };
+
+  const handleDown = () => {
+    if (disabled) return;
+    setValue(Number(value) - step);
+  };
+
+  useEffect(() => {
+    if (isNaN(Number(props.value))) return;
+    setValue(props.value || '');
+  }, [props.value]);
+
+  useEffect(() => {
+    setFocus(props.focus);
+  }, [props.focus]);
+
   return (
     <div
       className={clsx(className, `${prefix}-inputNumber`, {
-        [`${prefix}-inputNumber-default`]: !type && !disabled,
-        [`${prefix}-inputNumber-${type}`]: type,
+        [`${prefix}-inputNumber-focus`]: focus,
         [`${prefix}-inputNumber-disabled`]: disabled,
-        [`${prefix}-inputNumber-${size}`]: size,
       })}
-      style={{ margin: interval }}
-      onClick={handleClick}
-      {...prop}
+      style={style}
     >
-      {icon && <Icon name={icon} />}
-      <span>{children}</span>
+      {before && <div className={clsx(`${prefix}-inputNumber-before`)}>{before}</div>}
+      {icon && <div className={clsx(`${prefix}-inputNumber-icon`)}>{icon}</div>}
+      <div className={clsx(`${prefix}-inputNumber-content`)}>
+        <input
+          name={name}
+          value={value}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onInput={handleInput}
+          disabled={disabled}
+          maxLength={maxLength}
+          placeholder={placeholder || '请输入'}
+          ref={refEl}
+          autoComplete="off"
+        />
+      </div>
+      <div className={clsx(`${prefix}-inputNumber-handler`)}>
+        <span
+          className={clsx(`${prefix}-inputNumber-handler-btn`, `${prefix}-inputNumber-handler-up`)}
+          onClick={handleUp}
+        >
+          <Icon size={10} name="shang" />
+        </span>
+        <span
+          className={clsx(
+            `${prefix}-inputNumber-handler-btn`,
+            `${prefix}-inputNumber-handler-down`,
+          )}
+          onClick={handleDown}
+        >
+          <Icon size={10} name="xia" />
+        </span>
+      </div>
+      {affterIcon && <div className={clsx(`${prefix}-inputNumber-affterIcon`)}>{affterIcon}</div>}
+      {after && <div className={clsx(`${prefix}-inputNumber-after`)}>{after}</div>}
     </div>
   );
 }
 
-export default InputNumber;
+export default React.forwardRef(InputNumber);
