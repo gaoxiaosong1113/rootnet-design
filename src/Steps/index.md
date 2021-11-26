b2110a257fa8e66dc52f4ab0735f7a0c501129db
魏前行 <weiqianxing@croot.com>
2021-11-26 10:00:20 +0800

    增加步骤条组件

--- src/Steps/index.md
+++ src/Steps/index.md
@@ -9,11 +9,177 @@ group:
 
 ## Steps 步骤条
 
-示例：
+基础用法
 
 ```tsx
 import React from 'react';
-import { Foo } from 'rootnet-design';
+import { Steps, Grid, Icon, Button, Input, Checkbox } from 'rootnet-design';
+const { Row, Col } = Grid;
+const { Step } = Steps;
 
-export default () => <span>First Demo</span>;
+export default () => {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        <Steps
+          current={1}
+          onChange={() => {
+            alert(1);
+          }}
+        >
+          <Step
+            title="正在处理"
+            status="wait"
+            description="这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案"
+          />
+          <Step title="正在处理" description="这里是描述文案" />
+          <Step title="正在处理" status="finish" description="这里是描述文案" />
+          <Step title="正在处理" status="error" description="" />
+          <Step title="自定义图标" icon={<Icon name="liangduanduiqi" />} description="" />
+        </Steps>
+      </Col>
+    </Row>
+  );
+};
 ```
+
+点状步骤条
+
+```tsx
+import React from 'react';
+import { Steps, Grid, Icon, Button, Input, Checkbox } from 'rootnet-design';
+const { Row, Col } = Grid;
+const { Step } = Steps;
+
+export default () => {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        <Steps
+          type="dot"
+          current={1}
+          onChange={() => {
+            alert(1);
+          }}
+        >
+          <Step
+            title="正在处理"
+            status="wait"
+            description="这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案"
+          />
+          <Step title="正在处理" description="这里是描述文案" />
+          <Step title="正在处理" status="finish" description="这里是描述文案" />
+          <Step title="正在处理" status="error" description="" />
+          <Step title="自定义图标" icon={<Icon name="liangduanduiqi" />} description="" />
+        </Steps>
+      </Col>
+    </Row>
+  );
+};
+```
+
+竖直步骤条
+
+```tsx
+import React from 'react';
+import { Steps, Grid, Icon, Button, Input, Checkbox } from 'rootnet-design';
+const { Row, Col } = Grid;
+const { Step } = Steps;
+
+export default () => {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={12}>
+        <Steps
+          direction="vertical"
+          current={1}
+          onChange={() => {
+            alert(1);
+          }}
+        >
+          <Step
+            title="正在处理"
+            status="wait"
+            description="这里是描述文案这里是描述文案这里是描述这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案"
+          />
+          <Step title="正在处理" description="这里是描述文案" />
+          <Step title="正在处理" status="finish" description="这里是描述文案" />
+          <Step title="正在处理" status="error" description="" />
+          <Step title="自定义图标" icon={<Icon name="liangduanduiqi" />} description="" />
+        </Steps>
+      </Col>
+      <Col span={12}>
+        <Steps
+          direction="vertical"
+          type="dot"
+          current={1}
+          onChange={() => {
+            alert(1);
+          }}
+        >
+          <Step
+            title="正在处理"
+            status="wait"
+            description="这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案"
+          />
+          <Step title="正在处理" description="这里是描述文案" />
+          <Step title="正在处理" status="finish" description="这里是描述文案" />
+          <Step title="正在处理" status="error" description="" />
+          <Step title="自定义图标" icon={<Icon name="liangduanduiqi" />} description="" />
+        </Steps>
+      </Col>
+    </Row>
+  );
+};
+```
+
+自定义步骤条
+
+```tsx
+import React from 'react';
+import { Steps, Grid, Icon, Button, Input, Checkbox } from 'rootnet-design';
+const { Row, Col } = Grid;
+const { Step } = Steps;
+
+export default () => {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={12}>
+        <Steps
+          direction="vertical"
+          current={2}
+          customDotStatus={true}
+          onChange={() => {
+            alert(1);
+          }}
+        >
+          <Step
+            customDot={<Checkbox></Checkbox>}
+            title="已完成"
+            description="这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案这里是描述文案"
+          />
+          <Step title="正在处理" description="这里是描述文案" />
+          <Step
+            disabled
+            title={<Button type="primary">自定义标题</Button>}
+            description={
+              <div style={{ margin: '12px 0 ' }}>
+                <Input />
+              </div>
+            }
+          />
+          <Step
+            title="步骤错误"
+            status="error"
+            description={<Button type="primary">审核</Button>}
+          />
+          <Step title="自定义图标" icon={<Icon name="liangduanduiqi" />} description="" />
+          <Step title="正在处理" description="" />
+        </Steps>
+      </Col>
+    </Row>
+  );
+};
+```
+
+<API />
--- src/Steps/index.tsx
+++ src/Steps/index.tsx
@@ -1,5 +1,5 @@
 // 引入react依赖
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo, useRef, useImperativeHandle } from 'react';
 import ReactDOM from 'react-dom';
 
 // 引入第三方依赖
@@ -10,7 +10,7 @@ import clsx from 'clsx';
 import './index.less';
 
 // 引入配置文件
-import { prefix } from '../config';
+import { prefix, primaryColor, fullBrightColor } from '../config';
 
 // 引入组件
 import { Icon } from '../index';
@@ -22,79 +22,219 @@ export interface StepsProps {
    */
   className?: string;
   style?: Object;
-  children?: ReactNode;
+  children?: any;
 
   /**
-   * @description      按钮的类型
+   * @description      节点样式的类型
+   * @default           default
+   */
+  type?: 'default' | 'dot' | 'navigation';
+
+  /**
+   * @description      显示方向
+   * @default           horizontal
+   */
+  direction?: 'vertical' | 'horizontal';
+
+  /**
+   * @description      当前步骤数
+   * @default           1
+   */
+  current?: number;
+
+  /**
+   * @description      点击步骤回调
    * @default           -
    */
-  type?: string;
-
-  /**
-   * @description      需要显示的图标
-   * @default           -
-   */
-  icon?: string;
-
-  /**
-   * @description      是否禁用按钮
-   * @default           false
-   */
-  disabled?: boolean;
-
-  /**
-   * @description      Steps点击事件
-   * @default           -
-   */
-  onClick?: Function;
-
-  /**
-   * @description      Steps左右的间隔
-   * @default           -
-   */
-  interval?: string;
+  onChange?: string;
 
   /**
    * @description      Steps的尺寸
    * @default           -
    */
   size?: string;
+
+  /**
+   * @description      是否自定义节点前内容
+   * @default           false
+   */
+  customDotStatus?: Boolean;
+}
+
+export interface StepProps {
+  /**
+   * @description      类名
+   * @default           -
+   */
+  className?: string;
+  style?: Object;
+  index?: any;
+  current?: any;
+  type?: any;
+  onClick?: Function;
+  direction?: any;
+  customDotStatus?: Boolean;
+
+  /**
+   * @description      onChange 回调中作为参数。
+   * @default           -
+   */
+  id?: string;
+
+  /**
+   * @description      当前节点状态 未设置时根据index和current判断
+   * @default           -
+   */
+  status?: 'wait' | 'process' | 'finish' | 'error';
+
+  /**
+   * @description      节点标题
+   * @default           -
+   */
+  title?: string;
+
+  /**
+   * @description      节点描述
+   * @default           -
+   */
+  description?: string;
+
+  /**
+   * @description      当前节点被被禁用
+   * @default           false
+   */
+  disabled?: boolean;
+
+  /**
+   * @description      自定义节点icon
+   * @default           -
+   */
+  icon?: any;
+
+  /**
+   * @description      自定义节点前内容，如自定义必须将customDotStatus改为true
+   * @default           -
+   */
+  customDot?: any;
 }
 
 function Steps(props: StepsProps) {
   const {
     className,
-    type,
-    icon,
-    disabled,
+    style,
     children,
-    onClick,
-    interval,
+    type = 'default',
     size,
+    current,
+    onChange,
+    direction = 'horizontal',
+    customDotStatus = false,
     ...prop
   } = props;
 
-  function handleClick() {
-    if (!disabled && onClick) {
-      onClick();
-    }
-  }
   return (
     <div
       className={clsx(className, `${prefix}-steps`, {
-        [`${prefix}-steps-default`]: !type && !disabled,
+        [`${prefix}-steps-${direction}`]: direction,
         [`${prefix}-steps-${type}`]: type,
-        [`${prefix}-steps-disabled`]: disabled,
+        [`${prefix}-steps-click`]: typeof onChange === 'function',
         [`${prefix}-steps-${size}`]: size,
       })}
-      style={{ margin: interval }}
-      onClick={handleClick}
-      {...prop}
+      style={style}
     >
-      {icon && <Icon name={icon} />}
-      <span>{children}</span>
+      {React.Children.map(children, (item: any, index) => {
+        index += 1;
+        return (
+          item &&
+          React.cloneElement(item, {
+            type,
+            index,
+            current,
+            direction,
+            customDotStatus,
+            onClick: onChange,
+          })
+        );
+      })}
     </div>
   );
 }
+
+export function Step(props: StepProps, ref: any) {
+  const {
+    className,
+    style,
+    id,
+    title,
+    description,
+    status,
+    index,
+    current,
+    disabled,
+    onClick,
+    type,
+    icon,
+    direction,
+    customDotStatus,
+    customDot,
+  } = props;
+
+  const refEl = useRef(null);
+
+  const currentStatus = useMemo(() => {
+    if (status) return status;
+    if (current < index) return 'wait';
+    if (current === index) return 'process';
+    if (current > index) return 'finish';
+    return 'error';
+  }, [index, current]);
+
+  useImperativeHandle(ref, () => refEl.current);
+
+  function renderIconNode(currentStatus: any) {
+    if (type === 'dot') return null;
+
+    let iconContent = index;
+
+    if (icon) {
+      iconContent = icon;
+    } else if (currentStatus === 'finish') {
+      iconContent = <Icon color={primaryColor} name="zhengque1" />;
+    } else if (currentStatus === 'error') {
+      iconContent = <Icon color={fullBrightColor} name="cuowu1" />;
+    }
+
+    return <div className={`${prefix}-step-item-icon-i`}>{iconContent}</div>;
+  }
+
+  const iconNode = renderIconNode(currentStatus);
+
+  function handelStep() {
+    onClick && !disabled && index !== current && onClick(index, id);
+  }
+
+  return (
+    <div
+      ref={refEl}
+      className={clsx(className, `${prefix}-step-item`, `${prefix}-step-item-${currentStatus}`, {
+        [`${prefix}-step-item-disabled`]: disabled,
+        [`${prefix}-step-item-active`]: index === current,
+      })}
+      style={style}
+    >
+      {customDotStatus && <div style={{ minWidth: '24px' }}>{customDot}</div>}
+      <div style={{ display: 'flex', position: 'relative' }}>
+        {direction === 'vertical' && <div className={`${prefix}-step-item-line`}></div>}
+        <div className={`${prefix}-step-item-icon`}>{iconNode}</div>
+        <div className={`${prefix}-step-item-content`} onClick={handelStep}>
+          <div className={`${prefix}-step-item-title`}>{title}</div>
+          {description && <div className={`${prefix}-step-item-description`}>{description}</div>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Steps.Step = React.forwardRef(Step);
 
 export default Steps;
