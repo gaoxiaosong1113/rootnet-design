2cb6228a411e796519f0efe554258043d2d78fbe
Gaoxs <gaoxiaosong@huoban.com>
2023-12-12 15:27:55 +0800

    feat: 修改请求路径

--- .umirc.ts
+++ .umirc.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'Rootnet Design',
-  base: process.env.NODE_ENV === 'production' ? '/component/' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/component/' : '/',
-  favicon: process.env.NODE_ENV === 'production' ? '/component/logo.png' : '/logo.png',
-  logo: process.env.NODE_ENV === 'production' ? '/component/logo.png' : '/logo.png',
+  base: process.env.NODE_ENV === 'production' ? '/components/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/components/' : '/',
+  favicon: process.env.NODE_ENV === 'production' ? '/components/logo.png' : '/logo.png',
+  logo: process.env.NODE_ENV === 'production' ? '/components/logo.png' : '/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   history: {
